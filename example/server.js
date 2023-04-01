191a09ae25fbf51df18fd1cdd8168b4e7266134e
youssef io <youssef@gmail.com>
2023-04-01 08:14:46 +0000

    looks good

--- example/server.js
+++ example/server.js
@@ -37,6 +37,19 @@ function generateRandomString(length) {
   return crypto.randomBytes(length).toString("hex");
 }
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+let _anonyme_login_colors = [
+  "text-red-300",
+  "text-blue-300",
+  "text-gray-300",
+  "text-yello-300",
+  "text-slate-300",
+  "text-lime-300",
+]
+
 const uri =
   "mongodb+srv://" +
   username +
@@ -51,7 +64,7 @@ app.use(cookieParser());
 
 const UID = 'u-s4t2ud-73654fe2b860ec16aec83c32f324d0d3b6783f0b94ed7849c0ab557b0fb2b111';
 const SECRET = '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';
-const redURI = 'http://10.12.6.6:3000/api';
+const redURI = 'http://localhost:3000/api';
 let token = '';
 
 passport.use(new OAuth2Strategy({
@@ -100,15 +113,18 @@ app.get('/api',
             _rdm: _rdm,
             _name: response.data.displayname,
             _login: response.data.login,
+            _login_color: _anonyme_login_colors[getRandomInt(6)],
             _login_id: response.data.id,
             _image_link: response.data.image.link,
+            _anonyme_login: 'User_' + generateRandomString(4),
+            _anonyme_login_color: _anonyme_login_colors[getRandomInt(6)],
           });
         } catch {
           res.render("error", { error: "Error wlah ma n3rf" });
         }
       }
       set_data();
-      res.redirect('./dashboard');
+      res.redirect('/dashboard');
     }
     get_me_auth();
   });
@@ -130,7 +146,7 @@ app.get("/login", (req, res) => {
           res.redirect('/dashboard');
         }
       } catch {
-        res.redirect("./");
+        res.redirect("/");
       }
     }
     get_data();
@@ -140,31 +156,14 @@ app.get("/login", (req, res) => {
   }
 });
 
-app.get("/dashboard", (req, res) => {
-  async function get_data() {
-    try {
-      const database = client.db("db_data");
-      const collection = database.collection("coll_users");
-      const user_ = await collection.find({ _rdm: req.cookies._rdm }).toArray();
-      if (user_.length == 0) {
-        res.redirect('./');
-      }
-      else {
-        res.render("dashboard");
-      }
-    } catch {
-      res.redirect('./');
-    }
-  }
-  get_data();
-});
+
 
 app.get("/", (req, res) => {
   if (req.cookies._rmd == null) {
-    res.redirect("./login");
+    res.redirect("/login");
   }
   else {
-    res.redirect("./dashboard");
+    res.redirect("/dashboard");
   }
 })
 
@@ -179,12 +178,64 @@ app.get('/login/auth', (req, res) => {
         res.redirect(redURI_login);
       }
       else {
-        res.redirect('./');
+        res.redirect('/');
       }
     } catch {
-      res.redirect('./');
+      res.redirect('/');
     }
   }
   get_data();
 });
 
+
+
+app.get("/dashboard", (req, res) => {
+  res.redirect("/dashboard/general")
+});
+
+app.get("/dashboard/general", (req, res) => {
+  async function get_data() {
+    try {
+      const database = client.db("db_data");
+      const coll_users = database.collection("coll_users");
+      const coll_messages = database.collection("coll_messages");
+      const user_ = await coll_users.find({ _rdm: req.cookies._rdm }).toArray();
+      const messages_ = await coll_messages.find({_channel: "general"}).toArray();
+      if (user_.length == 0) {
+        res.redirect('/');
+      }
+      else {
+        res.render("dashboard", {
+          data: messages_,
+          // data: [
+          //   {
+          //     _sender_display: "ylabrahm",
+          //     _message: "Youssefd ioasfwjfn is  the best youssef io in the weorld",
+          //     _sender_display_color: _anonyme_login_colors[getRandomInt(6)],
+          //     _likes: 5,
+          //     _dislikes: 11,
+          //   },
+          //   {
+          //     _sender_display: "aasselma",
+          //     _message: "Ahmed ahmed ahemd, that's it man don't argue with me Ahmed ahmed ahemd, that's it man don't argue with me Ahmed ahmed ahemd, that's it man don't argue with me Ahmed ahmed ahemd, that's it man don't argue with me Ahmed ahmed ahemd, that's it man don't argue with me Ahmed ahmed ahemd, that's it man don't argue with me Ahmed ahmed ahemd, that's it man don't argue with me Ahmed ahmed ahemd, that's it man don't argue with me ",
+          //     _sender_display_color: _anonyme_login_colors[getRandomInt(6)],
+          //     _likes: 2,
+          //     _dislikes: 0,
+          //   },
+          //   {
+          //     _sender_display: "sbellafr",
+          //     _message: "Slay_   maaaxxx sfyous  max max maxn minyou Slay_   maaaxxx sfyous  max max maxn minyou Slay_   maaaxxx sfyous  max max maxn minyou Slay_   maaaxxx sfyous  max max maxn minyou Slay_   maaaxxx sfyous  max max maxn minyou ",
+          //     _sender_display_color: _anonyme_login_colors[getRandomInt(6)],
+          //     _likes: 7,
+          //     _dislikes: 3,
+          //   },
+          // ]
+        }
+        );
+      }
+    } catch {
+      res.redirect('/');
+    }
+  }
+  get_data();
+});
